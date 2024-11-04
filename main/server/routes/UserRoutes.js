6ab1647d65400a78023d875d3a5674b65235633a
Aarya Balwadkar <142713127+AaryaBalwadkar@users.noreply.github.com>
2024-11-04 13:46:14 +0530

    Run client and server inside socket-demo

--- main/server/routes/UserRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import protectRoute from "../middleware/ProtectRoute.js";
-import { getUsersForSidebar } from "../controller/UserController.js";
-
-const router = express.Router();
-
-router.get("/", protectRoute, getUsersForSidebar);
-
-export default router;
